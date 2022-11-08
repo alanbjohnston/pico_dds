b2892ea8e4209e0459957f45dd587267aad6f773
alanbjohnston <alan.b.johnston@gmail.com>
2022-11-08 11:04:36 -0500

    clear irq at end

--- pico_dds.cpp
+++ pico_dds.cpp
@@ -79,7 +79,7 @@ void dds_begin() {
 }
 
 void dds_pwm_interrupt_handler() {
-  pwm_clear_irq(pwm_gpio_to_slice_num(DDS_PWM_PIN)); 
+//  pwm_clear_irq(pwm_gpio_to_slice_num(DDS_PWM_PIN)); 
 
   if (dds_enable) {
     if (dds_counter++ > 9) {  
@@ -104,7 +104,7 @@ void dds_pwm_interrupt_handler() {
      pwm_set_gpio_level(DDS_PWM_PIN,0);
   } 
 
-//    pwm_clear_irq(pwm_gpio_to_slice_num(DDS_PWM_PIN)); 
+    pwm_clear_irq(pwm_gpio_to_slice_num(DDS_PWM_PIN)); 
 }
 
 
